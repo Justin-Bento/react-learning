5f3f6d189dea72a0db15e32b22dbd5bfc470773f
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2023-05-09 12:04:51 +0100

    imported button component into main app

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import MyButton from './components/MyButton'
 
 export default function page() {
   return (
-    <div>page</div>
+    <div>
+      <MyButton>Button Component</MyButton>
+    </div>
   )
 }
